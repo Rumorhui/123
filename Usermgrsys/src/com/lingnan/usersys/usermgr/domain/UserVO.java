9084befa4e26c7a91136323168b5601416c9c8ef
Administrator <Administrator@LS--20120726UMG>
2018-07-30 09:20:08 +0800

    123

---
+++ Usermgrsys/src/com/lingnan/usersys/usermgr/domain/UserVO.java
@@ -0,0 +1,49 @@
+package com.lingnan.usersys.usermgr.domain;
+
+public class UserVO {
+	private String U_ID;
+	private String U_name;
+	private String U_pass;
+	private String U_sex;
+	private String U_power;
+	private String U_del;
+	
+	public String getU_ID() {
+		return U_ID;
+	}
+	public void setU_ID(String u_ID) {
+		U_ID = u_ID;
+	}
+	public String getU_name() {
+		return U_name;
+	}
+	public void setU_name(String u_name) {
+		U_name = u_name;
+	}
+	public String getU_pass() {
+		return U_pass;
+	}
+	public void setU_pass(String u_pass) {
+		U_pass = u_pass;
+	}
+	public String getU_sex() {
+		return U_sex;
+	}
+	public void setU_sex(String u_sex) {
+		U_sex = u_sex;
+	}
+	public String getU_power() {
+		return U_power;
+	}
+	public void setU_power(String u_power) {
+		U_power = u_power;
+	}
+	public String getU_del() {
+		return U_del;
+	}
+	public void setU_del(String u_del) {
+		U_del = u_del;
+	}
+	
+
+}
